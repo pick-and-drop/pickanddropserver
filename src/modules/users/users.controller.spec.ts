0937c9b8916bbba32a64cfcd8cf0408d13880705
Amin Ogarrio <amin.ogarrio@gmail.com>
2021-05-22 23:48:44 -0700

    Added user controller testing with super test.

--- src/modules/users/users.controller.spec.ts
+++ src/modules/users/users.controller.spec.ts
@@ -1,3 +1,5 @@
+import * as request from 'supertest';
+import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { Repository } from 'typeorm';
 import * as faker from 'faker';
@@ -5,10 +7,9 @@ import * as faker from 'faker';
 import { TestingProviderModule } from '../../providers/testing/provider.module';
 import { UsersModule } from './users.module';
 import { User } from './entities/user.entity';
-import UsersController from './users.controller';
 
-describe('UserController', () => {
-  let usersController: UsersController;
+describe('UsersController', () => {
+  let app: INestApplication;
   let userRepository: Repository<User>;
 
   beforeAll(async () => {
@@ -17,10 +18,12 @@ describe('UserController', () => {
     }).compile();
 
     userRepository = moduleRef.get('UserRepository');
-    usersController = moduleRef.get<UsersController>(UsersController);
+
+    app = moduleRef.createNestApplication();
+    await app.init();
   });
 
-  describe('all', () => {
+  describe('GET /users', () => {
     beforeEach(async () => {
       await userRepository.insert({
         name: faker.name.findName(),
@@ -39,20 +42,19 @@ describe('UserController', () => {
       });
     });
 
+    it('responds 3 users', async () => {
+      const response = await request(app.getHttpServer()).get('/users');
+
+      expect(response.status).toBe(200);
+      expect(response.body.length).toBe(3);
+    });
+
     afterEach(async () => {
       await userRepository.clear();
     });
+  });
 
-    it('should return 3 users', async () => {
-      const users = await usersController.index();
-
-      expect(users.length).toBe(3);
-    });
-
-    it('should return 3 users', async () => {
-      const users = await usersController.index();
-
-      expect(users.length).toBe(3);
-    });
+  afterAll(async () => {
+    await app.close();
   });
 });
