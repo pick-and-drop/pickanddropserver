11324dc25278bfad897238b0e8b03d4a9850ee98
Amin Ogarrio <amin.ogarrio@gmail.com>
2021-06-20 13:51:43 -0700

    Fixed request.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 
 import { UsersModule } from './modules/users/users.module';
 import { PostgresDatabaseProviderModule } from './providers/postgres/provider.module';
-import { RequestsModule } from './requests/requests.module';
 
 const ENV = process.env.NODE_ENV;
 
@@ -14,7 +13,6 @@ const ENV = process.env.NODE_ENV;
     }),
     PostgresDatabaseProviderModule,
     UsersModule,
-    RequestsModule,
   ],
 })
 export class AppModule {}
